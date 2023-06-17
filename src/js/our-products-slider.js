f77c3060550038b0aff497ae33affcc10c7ba755
Dmytro Sayko <saykodmytro@icloud.com>
2023-06-17 19:59:09 +0300

    section-our-products

---
+++ src/js/our-products-slider.js
@@ -0,0 +1,23 @@
+let swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 1,
+  spaceBetween: 18,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 18,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+  },
+  autoplay: {
+    delay: 1500,
+  },
+});
